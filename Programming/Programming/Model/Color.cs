9cd7e3b79aee620c5bc9a1850c53f6a3aed32fe7
DanS0137 <grotovartem6@gmail.com>
2024-02-26 18:39:01 +0700

    Созданы перечисления

---
+++ Programming/Programming/Model/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    internal enum Color
+    {
+        White,
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Violet,
+        Black,
+    }
+}
---
+++ Programming/Programming/Model/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    internal enum Genre
+    {
+        Drama,
+        Comedy,
+        ActionFilm,
+        Thriller,
+        Horror,
+        Blockbuster,
+    }
+}
