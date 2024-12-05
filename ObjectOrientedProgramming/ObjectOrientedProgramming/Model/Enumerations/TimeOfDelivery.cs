b16070150ac0061a6111b8f8d11a29003a622704
User <User@WIN-5BMCI9MCAJF>
2024-12-05 12:01:39 +0700

    Добавлен класс PriorityOrder.

---
+++ ObjectOrientedProgramming/ObjectOrientedProgramming/Model/Enumerations/TimeOfDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming.Model.Enumerations
+{
+    public enum TimeOfDelivery
+    {
+        NineToEleven,
+        ElevenToThirteen,
+        ThirteenToFifteen,
+        FifteenToSeventeen,
+        SeventeenToNineteen,
+        NineteenToTwentyOne
+    }
+}
